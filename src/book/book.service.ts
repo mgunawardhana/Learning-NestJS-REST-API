602a9abf31d96f97a6d092332a98fa0bb8e22b9d
mgunawardhana <mrgunawardhana27368@gmail.com>
2024-11-26 20:05:42 +0530

    content updated

---
+++ src/book/book.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { Book } from './schemas/book.schemas';
+import * as mongoose from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class BookService {
+  constructor(
+    @InjectModel(Book.name) private bookModel: mongoose.Model<Book>,
+  ) {}
+
+  async findAll(): Promise<Book[]> {
+    return this.bookModel.find();
+  }
+
+}
